fce4da4439beeca7b24cab01de88d3c44bb1f2e8
Mr.Pat <kcirtap.asis@gmail.com>
2022-05-04 20:20:55 +0800

    chance BrowerRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 
 ReactDOM.render(
-  <Router base = "/">
+  <HashRouter base = "/">
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
